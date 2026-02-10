c3a6a91387cf271aab868725bfa299153242546b
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2026-02-10 22:59:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-import { auth, db } from "./firebase.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
 import {
+  getAuth,
   createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  onAuthStateChanged
+  signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
-let startTime;
+const firebaseConfig = {
+  apiKey: "AIzaSyCQZ36IQVFEm-rqyD0WlPO23Sf8Lih5Fo8",
+  authDomain: "study-zone-438c4.firebaseapp.com",
+  projectId: "study-zone-438c4",
+  storageBucket: "study-zone-438c4.firebasestorage.app",
+  messagingSenderId: "320895535749",
+  appId: "1:320895535749:web:cab35a214ef7c22ae2f7ec"
+};
 
-document.getElementById("signup").onclick = () => {
-  const email = email.value;
-  const password = password.value;
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-  createUserWithEmailAndPassword(auth, email, password)
+// DOM elements
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const signupBtn = document.getElementById("signup");
+const loginBtn = document.getElementById("login");
+
+// SIGN UP
+signupBtn.addEventListener("click", () => {
+  createUserWithEmailAndPassword(auth, email.value, password.value)
     .then(() => alert("Account created 🔥"))
     .catch(err => alert(err.message));
-};
-
-document.getElementById("login").onclick = () => {
-  signInWithEmailAndPassword(auth, email.value, password.value)
-    .catch(err => alert(err.message));
-};
-
-onAuthStateChanged(auth, user => {
-  if (user) {
-    auth.style.display = "none";
-    dashboard.style.display = "block";
-  }
 });
 
-document.getElementById("start").onclick = () => {
-  startTime = Date.now();
-};
-
-document.getElementById("stop").onclick = () => {
-  const mins = Math.floor((Date.now() - startTime) / 60000);
-  document.getElementById("time").innerText = `${mins} mins studied`;
-};
+// LOGIN
+loginBtn.addEventListener("click", () => {
+  signInWithEmailAndPassword(auth, email.value, password.value)
+    .then(() => alert("Logged in 😤"))
+    .catch(err => alert(err.message));
+});
